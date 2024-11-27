2ea77a89addd14af37f104013d0c7442011fb69f
hugocotoflorez <hugocoto100305@gmail.com>
2024-11-27 17:57:07 +0100

    aliases + kill processes with ^C

--- README.md
+++ README.md
@@ -11,6 +11,14 @@
 
 ## Things that (may) work
 
+- Config file: ./.hsllrc
 - Path variables expansion
 - Prompt (colors and path variables)
 - cd (all except ../.. and - and advanced stuff)
+- Aliases
+
+## How to access shell variables
+
+At least in my machine env variables such as HOST
+are not accessible from hsll. I can fix it by
+placing `export HOST` in the parent shell config.
--- src/shell.c
+++ src/shell.c
@@ -11,14 +11,12 @@
 #define LINELEN        1023
 #define DEFAULT_PROMPT ">> "
 
-int   __quit = 0;
-char *prompt = DEFAULT_PROMPT;
-
-void
-__quit_handler()
-{
-    __quit = 1;
-}
+// If __quit is 1, the shell exits in the next loop step
+int       __quit = 0;
+char     *prompt = DEFAULT_PROMPT;
+HcfField *aliases;
+// PID of proc in execution or 0 if nothing is running
+int child = 0;
 
 char *
 __expand_variables(char *str)
@@ -38,6 +36,65 @@ __prompt()
     fflush(stdout);
     free(s);
 }
+void
+__quit_handler()
+{
+    __quit = 1;
+}
+
+/* Send sigterm to proc in execution */
+void
+__kill_child()
+{
+    if (child)
+    {
+        kill(child, SIGTERM);
+        /* ^C is printed in the same line as the prompt, this solve that */
+        puts("");
+    }
+    else
+    {
+        puts("");
+        __prompt();
+    }
+}
+
+char *
+__expand_alias(char *str)
+{
+    int   index;
+    char *alias;
+    char  temp;
+
+    if (!aliases)
+        return str;
+
+    index = strcspn(str, " \n\r");
+    temp  = str[index];
+
+    if (temp)
+    {
+        str[index] = '\0';
+        if ((alias = hcf_get_value(*aliases, str)))
+        {
+            memmove(str + strlen(alias) + 1, str + index + 1,
+                    strlen(str + index + 1) + 1);
+            memmove(str, alias, strlen(alias));
+            str[strlen(alias)] = temp;
+        }
+        else
+            str[index] = temp;
+    }
+
+    else
+    {
+        if ((alias = hcf_get_value(*aliases, str)))
+        {
+            strcpy(str, alias);
+        }
+    }
+    return str;
+}
 
 int
 hsll_init()
@@ -51,14 +108,17 @@ hsll_init()
         return -1;
 
     shell_opts = hcf_load(".hsllrc");
+    aliases    = hcf_get_field(shell_opts, "aliases");
     prompt     = hcf_get(shell_opts, "options", "prompt") ?: prompt;
 
     assert(signal(SIGTERM, __quit_handler) != SIG_ERR);
+    assert(signal(SIGINT, __kill_child) != SIG_ERR);
 
     while (!__quit)
     {
         __prompt();
         get_input_line(line, LINELEN, stdin);
+        __expand_alias(line);
         __expand_variables(line);
         execute(s = __split(line), NULL, NULL);
         free(s);
@@ -75,7 +135,7 @@ execute(char *command[const], int *__stdin, int *__stdout)
     if (!(command && command[0]))
         return -1;
 
-    switch (fork())
+    switch (child = fork())
     {
         case -1:
             perror("fork");
@@ -107,6 +167,7 @@ execute(char *command[const], int *__stdin, int *__stdout)
                 exec_builtin_command(command);
 
             wait(NULL);
+            child = 0;
     }
     return 0;
 }
